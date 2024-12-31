e47e76087c226ad86a4a05dbe7de442e402ac5e5
Błażejek Development <nevix@tutamail.com>
2024-12-31 12:40:55 +0100

    Change a icon and Facebook Container

--- src/SocialMediaContainer/SocialMediaContainer.jsx
+++ src/SocialMediaContainer/SocialMediaContainer.jsx
@@ -1,7 +1,7 @@
 import SocialMedia from "../SocialMedia/SocialMedia";
 import styles from "./SocialMediaContainer.module.css";
 import ImageOfGitHub from "../assets/GitHub.jpg";
-import ImageOfFacebook from "../assets/Facebook.svg";
+//import ImageOfFacebook from "../assets/Facebook.svg";
 import ImageOfDiscord from "../assets/Discord.svg";
 
 function SocialMediaContainer() {
@@ -9,21 +9,21 @@ function SocialMediaContainer() {
     <div className={styles.container}>
       <SocialMedia
         name="GitHub"
-        adres="/blazejekdevelopment"
+        adres="/BlazejekDevelopment"
         image={ImageOfGitHub}
         adresURL="https://github.com/BlazejekDevelopment"
       />
-      <SocialMedia
+      {/* <SocialMedia
         name="Facebook"
         adres="blazej-eikmannnn"
         image={ImageOfFacebook}
         adresURL="https://www.facebook.com/Anonim120?locale=pl_PL"
-      />
+      /> */}
       <SocialMedia
         name="Discord"
         adres="Błażejek Devlopment Community"
         image={ImageOfDiscord}
-        adresURL="https://discord.gg/NruzHPnr"
+        adresURL="https://discord.gg/sSSMWQXQ8m"
       />
     </div>
   );
